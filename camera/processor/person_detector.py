03aaa0ba5e3e0a4a8d21ccfa79f881f2beb349a5
shinzooon <39300094+shinyamizuno1008@users.noreply.github.com>
2018-11-07 20:30:24 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -1,18 +1,39 @@
 from __future__ import print_function
 from imutils.video.pivideostream import PiVideoStream
-from imutils.object_detection import non_max_suppression
 import imutils
 import time
 import numpy as np
 import cv2
 
+import os
+import sys
+import requests
+
+
+try:
+    SLACK_URL = os.environ['SLACK_URL']
+    SLACK_TOKEN = os.environ['SLACK_TOKEN']
+    SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
+except KeyError as e:
+    sys.exit('Couldn\'t find env: {}'.format(e))
 
 net = cv2.dnn.readNetFromCaffe('/home/pi/models/MobileNetSSD_deploy.prototxt',
         '/home/pi/models/MobileNetSSD_deploy.caffemodel')
+        
+
+def upload():
+    image = { 'file': open('hello.jpg', 'rb') }
+    payload = {
+        'filename': 'hello.jpg',
+        'token': SLACK_TOKEN,
+        'channels': [SLACK_CHANNEL],
+    }
+    requests.post(SLACK_URL, params=payload, files=image)
 
 
 class PersonDetector(object):
     def __init__(self, flip = True):
+        self.last_upload = time.time()
         self.vs = PiVideoStream(resolution=(800, 608)).start()
         self.flip = flip
         time.sleep(2.0)
@@ -59,5 +80,10 @@ class PersonDetector(object):
         
         if count > 0:
             print('Count: {}'.format(count))
+            elapsed = time.time() - self.last_upload
+            if elapsed > 60:
+                cv2.imwrite('hello.jpg', frame)
+                upload()
+                self.last_upload = time.time()
                 
         return frame
